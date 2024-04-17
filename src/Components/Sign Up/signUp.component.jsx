364b88efb68d15822bbac6ef855afa87f8b51f6b
Sunny Mishra <sunnymishra9007689708@gmail.com>
2024-04-17 17:15:30 +0530

    Form onChange operation done

--- src/Components/Sign Up/signUp.component.jsx
+++ src/Components/Sign Up/signUp.component.jsx
@@ -1,15 +1,37 @@
-import { Fragment, useContext } from 'react';
+import { Fragment, useContext, useState } from 'react';
 import { UserContext } from '../../Context/user.context';
+
+const defaultFieldValue = {
+    email: '',
+    password: ''
+}
 
 const SignUp = () => {
     const { setUserAuthType } = useContext(UserContext);
+    const [ formFieldValue, setFormFieldValue ] = useState(defaultFieldValue);
+    const { email, password } = formFieldValue;
+    console.log(formFieldValue);
+    const handleFormValueChange = (e)=>{
+        const {name, value} = e.target;
+        setFormFieldValue({...formFieldValue, [name]:value })
+    }
+
+    const onSubmitValue = ()=>{
+        //After sign Up operation will be here
+        resetFormField();
+    }
+
+    const resetFormField = ()=>{
+        setFormFieldValue(defaultFieldValue);
+    }
+
     return (
         <Fragment>
             <div className="auth-input-container">
-                <input className='auth-input' type='email' placeholder='Enter email' />
-                <input className='auth-input' type='password' placeholder='Enter password' />
+                <input className='auth-input' onChange={handleFormValueChange} type='email' placeholder='Enter email' name='email' value={email}/>
+                <input className='auth-input' onChange={handleFormValueChange} type='password' placeholder='Enter password' name='password' value={password} />
             </div>
-            <button className='auth-button'>Sign Up</button>
+            <button onClick={onSubmitValue} className='auth-button'>Sign Up</button>
             <p className='auth-text'>Have an account? <span onClick={()=> setUserAuthType('login')}>Sign In</span></p>
         </Fragment>
     );
